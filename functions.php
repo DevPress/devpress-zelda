ba2e81c64b7e35b28af9758a7bdc50802ec114b8
Devin Price <devin@wptheming.com>
2016-03-02 21:52:24 -0600

    Version 1.1.3

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@
 /**
  * The current version of the theme.
  */
-define( 'ZELDA_VERSION', '1.1.2' );
+define( 'ZELDA_VERSION', '1.1.3' );
 
 /**
  * Set the content width based on the theme's design and stylesheet.
